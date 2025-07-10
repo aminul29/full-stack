89ec8a447a546f2f90e6ac32e5d7cbe448a5ead3
aminul29 <89416308+aminul29@users.noreply.github.com>
2025-07-10 11:05:48 +0600

    add winston and morgan logger to the express crud app for effective debugging

--- backend-with-mongodb/02-express/index.js
+++ backend-with-mongodb/02-express/index.js
@@ -2,6 +2,10 @@
     // 01 Import the express module
     import express from 'express';
 
+    // logger-01: import the winston morgan logger
+    import logger from "./logger.js";
+    import morgan from "morgan";
+
     // 02 create an express app
     const app = express();
 
@@ -11,6 +15,26 @@
     // 05 use the express.json middleware to parse incoming requests with JSON payloads. 
     // this means convert the request body to a JavaScript object
     app.use(express.json());
+
+    // logger-02: use the morgan format to log the requests
+    const morganFormat = ":method :url :status :response-time ms";
+
+    // logger-03: use the morgan middleware to log the requests
+    app.use(
+    morgan(morganFormat, {
+        stream: {
+        write: (message) => {
+            const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+            };
+            logger.info(JSON.stringify(logObject));
+        },
+        },
+    })
+    );
     
     // 06 create an array to store the users data
     let userData = [];
@@ -31,6 +55,47 @@
     app.get('/users', (req, res) => {
         // 14 send the user data to the client
         res.status(200).send(userData);
+    });
+
+    // 15 read the user data with the get method to get a specific user
+    app.get('/users/:id', (req, res) => {
+        // 16 find the user with the given id that matches the id in the url.
+        const user = userData.find(targetUser => targetUser.id === parseInt(req.params.id));
+        // 17 if the user is not found
+        if(!user) {
+            return res.status(404).send('User not found');
+        }
+        // 18 If the user is found, send the user data to the client
+        res.status(200).send(user);
+    });
+
+
+    // 19 update the user data with the put method to update a specific user
+    app.put('/users/:id', (req, res) => {
+        // 20 find the user with the given id that matches the id in the url
+        const user = userData.find(targetUser => targetUser.id === parseInt(req.params.id));
+        // 21 if the user is not found
+        if(!user) {
+            return res.status(404).send('User not found');
+        }
+        // 22 get the user data from the request body
+        const {name, salary} = req.body;
+        user.name = name; // 23 update the user name
+        user.salary = salary; // 24 update the user salary
+        res.status(200).send(user); // 25 send the updated user data to the client
+    });
+
+    // 26 delete the user data with the delete method to delete a specific user
+    app.delete('/users/:id', (req, res) => {
+        // 27 find the user with the given id that matches the id in the url
+        const index = userData.findIndex(targetUser => targetUser.id === parseInt(req.params.id));
+        // 28 if the user is not found
+        if(index === -1) {
+            return res.status(404).send('User not found');
+        }
+        // 29 remove the user from the users array
+        userData.splice(index, 1);
+        res.status(200).send('User deleted');
     });
 
     // 04 start the server and listen to the port with a call back function
